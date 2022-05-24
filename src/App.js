233cab44796576c2f4282acbdb69d2c29b2c2cd3
Victor-Hernandez <victor.hernandez@coxautoinc.com>
2022-05-24 18:11:31 -0500

    Saving App.js changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   return (
